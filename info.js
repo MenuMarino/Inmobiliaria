844d0a65f481ccfe99d54c9f5378e4144696e956
MenuMarino <benjamin.diaz@utec.edu.pe>
2022-06-24 13:12:04 -0500

    Stand id in image, logo and title when pressed

---
+++ info.js
@@ -0,0 +1,455 @@
+var infoStands = {
+  A1: {
+    title: 'Arquimia',
+  },
+  A2: {
+    title: 'Vienna',
+  },
+  A3: {
+    title: 'San Charbel',
+  },
+  A4: {
+    title: 'Madrid',
+  },
+  A5: {
+    title: 'VYV',
+  },
+  A6: {
+    title: 'Sembra',
+  },
+  A7: {
+    title: 'My home',
+  },
+  A8: {
+    title: 'Cosapi',
+  },
+  A9: {
+    title: 'Global',
+  },
+  A10: {
+    title: 'EE',
+  },
+  A11: {
+    title: 'INARCO',
+  },
+  A12: {
+    title: 'Ciudaris',
+  },
+  A13: {
+    title: 'Fundamenta',
+  },
+  A14: {
+    title: 'Albamar',
+  },
+  A15: {
+    title: 'Grupo Sol',
+  },
+  A16: {
+    title: 'Senda',
+  },
+  A17: {
+    title: 'Stella',
+  },
+  A18: {
+    title: 'Activa',
+  },
+  A19: {
+    title: 'Grupo Lar',
+  },
+  A20: {
+    title: 'Inhouse',
+  },
+  A21: {
+    title: 'Ecohabitat',
+  },
+  A22: {
+    title: 'Evergran',
+  },
+  A23: {
+    title: 'La venturosa',
+  },
+  A24: {
+    title: 'Lugano',
+  },
+  A25: {
+    title: 'Grupo MG',
+  },
+  A26: {
+    title: 'La muralla',
+  },
+  A27: {
+    title: 'Paz Centenario',
+  },
+  A28: {
+    title: 'Danfra',
+  },
+  A29: {
+    title: 'Binda',
+  },
+  A30: {
+    title: 'Los Portales',
+  },
+  A31: {
+    title: 'Rumi',
+  },
+  A32: {
+    title: 'Menorca',
+  },
+  A33: {
+    title: 'Maquisistema',
+  },
+  A34: {
+    title: 'Pandero',
+  },
+  A35: {
+    title: 'Marcan',
+  },
+  A36: {
+    title: 'Akamai',
+  },
+  A37: {
+    title: 'Inmgenio',
+  },
+  A38: {
+    title: 'Los Portales HU',
+  },
+  B1: {
+    title: 'BBVA',
+  },
+  B2: {
+    title: '',
+  },
+  B3: {
+    title: 'Interbank',
+  },
+  B4: {
+    title: 'FMV',
+  },
+  M1: {
+    title: 'TYC',
+  },
+  M2: {
+    title: 'Caral',
+  },
+  M3: {
+    title: 'DKSA',
+  },
+  M4: {
+    title: 'Invent',
+  },
+  M5: {
+    title: 'Casa ideal',
+  },
+  M6: {
+    title: 'Edifica',
+  },
+  M7: {
+    title: 'Imagina',
+  },
+  M8: {
+    title: 'Urbana',
+  },
+  M9: {
+    title: 'Abril',
+  },
+  M10: {
+    title: 'Actual',
+  },
+  M11: {
+    title: 'Cantabria',
+  },
+  M12: {
+    title: 'Desarrolladora',
+  },
+  M13: {
+    title: 'Cissac',
+  },
+  M14: {
+    title: 'Invent',
+  },
+  M15: {
+    title: 'VYV',
+  },
+  M16: {
+    title: 'Brazil',
+  },
+  M17: {
+    title: 'Tale',
+  },
+  M18: {
+    title: 'Qualis',
+  },
+  M19: {
+    title: 'Capac',
+  },
+  M20: {
+    title: 'Abril',
+  },
+  M21: {
+    title: 'Arteco',
+  },
+  M22: {
+    title: 'Vitain',
+  },
+  S1: {
+    title: 'CYV',
+  },
+  S2: {
+    title: 'CYV',
+  },
+  S3: {
+    title: 'NOI',
+  },
+  S4: {
+    title: 'Promsal',
+  },
+  S5: {
+    title: 'Andiamo',
+  },
+  S6: {
+    title: 'Libre',
+  },
+  S7: {
+    title: 'Padova',
+  },
+  S8: {
+    title: 'Mirano',
+  },
+  S9: {
+    title: 'MAAS',
+  },
+  S10: {
+    title: 'Quark',
+  },
+  S11: {
+    title: 'HPC',
+  },
+  S12: {
+    title: 'HPC',
+  },
+  S13: {
+    title: 'HPC',
+  },
+  S14: {
+    title: 'TYC',
+  },
+  S15: {
+    title: 'TYC',
+  },
+  S16: {
+    title: 'TYC',
+  },
+  S17: {
+    title: 'Quatro',
+  },
+  S18: {
+    title: 'Quatro',
+  },
+  S19: {
+    title: 'Diegal',
+  },
+  S20: {
+    title: 'FAI',
+  },
+  S21: {
+    title: 'Rosiam',
+  },
+  S22: {
+    title: 'MCR',
+  },
+  S23: {
+    title: 'MCR',
+  },
+  S24: {
+    title: 'TER',
+  },
+  S25: {
+    title: 'Opcion',
+  },
+  S26: {
+    title: 'AF Constructenia',
+  },
+  S27: {
+    title: 'CP Building',
+  },
+  S28: {
+    title: 'IFI',
+  },
+  S29: {
+    title: 'IFI',
+  },
+  S30: {
+    title: 'Urbalima',
+  },
+  S31: {
+    title: 'Constructa',
+  },
+  S32: {
+    title: 'Grupo Norte',
+  },
+  S33: {
+    title: 'Grupo Norte',
+  },
+  S34: {
+    title: 'S&M',
+  },
+  S35: {
+    title: 'S&M',
+  },
+  S36: {
+    title: 'Multidepas',
+  },
+  S37: {
+    title: 'Neovida',
+  },
+  S38: {
+    title: 'In Urban',
+  },
+  S39: {
+    title: 'Alerces',
+  },
+  S40: {
+    title: 'Alerces',
+  },
+  S41: {
+    title: 'Octagon',
+  },
+  S42: {
+    title: 'Checor',
+  },
+  S43: {
+    title: 'Avitar',
+  },
+  S44: {
+    title: 'Eficaxx',
+  },
+  S45: {
+    title: 'Demega',
+  },
+  S46: {
+    title: 'OLED',
+  },
+  S47: {
+    title: 'Pisonay',
+  },
+  S48: {
+    title: 'Verones',
+  },
+  S49: {
+    title: 'Verones',
+  },
+  S50: {
+    title: 'Yoke',
+  },
+  S51: {
+    title: 'Puerta de tierra',
+  },
+  S52: {
+    title: 'Metropoli',
+  },
+  S53: {
+    title: 'Novoarte',
+  },
+  S54: {
+    title: 'Natturale',
+  },
+  S55: {
+    title: 'Natturale',
+  },
+  S56: {
+    title: 'Tangible',
+  },
+  S57: {
+    title: 'BE Inmobiliaria',
+  },
+  S58: {
+    title: 'DCI',
+  },
+  S59: {
+    title: 'Crearte',
+  },
+  S60: {
+    title: 'Centenario',
+  },
+  S61: {
+    title: 'Centenario',
+  },
+  S61a: {
+    title: 'Pionero',
+  },
+  S62: {
+    title: 'Edifikarte',
+  },
+  S63: {
+    title: 'Edifikarte',
+  },
+  S64: {
+    title: 'RB Arquitectura',
+  },
+  S65: {
+    title: 'Avenir',
+  },
+  S66: {
+    title: 'Avenir',
+  },
+  S67: {
+    title: 'Casa Bonita',
+  },
+  S68: {
+    title: 'Comunidad',
+  },
+  S69: {
+    title: 'Procity',
+  },
+  S70: {
+    title: 'Procity',
+  },
+  S71: {
+    title: 'NADLAN',
+  },
+  S72: {
+    title: 'NADLAN',
+  },
+  S73: {
+    title: 'ICasa',
+  },
+  S74: {
+    title: 'ICasa',
+  },
+  S75: {
+    title: 'Proyec',
+  },
+  S76: {
+    title: 'Proyec',
+  },
+  S77: {
+    title: 'Cygnus',
+  },
+  S78: {
+    title: 'Ortiz',
+  },
+  S79: {
+    title: 'GDC',
+  },
+  S80: {
+    title: 'Initalia',
+  },
+  S81: {
+    title: 'San Martin',
+  },
+  S82: {
+    title: 'Palace',
+  },
+  S83: {
+    title: 'Valico',
+  },
+  S84: {
+    title: 'Belgica',
+  },
+  S85: {
+    title: 'Ditrenzzo',
+  },
+  S86: {
+    title: 'Marte',
+  },
+};
--- scripts.js
+++ scripts.js
@@ -1,22 +1,12 @@
 var allActive = true;
-var distritos = {
-  miraflores: {
-    imgs: ['M1', 'A13', 'A19', 'S73'],
-    active: false,
-  },
-  san_borja: {
-    imgs: ['A16', 'A37', 'A21'],
-    active: false,
-  },
-  barranco: {
-    imgs: ['M22', 'A2', 'S18'],
-    active: false,
-  },
-};
 
 function showInfo(key) {
-  document.getElementById('infoTitle').textContent = 'Titulo' + key;
-  document.getElementById('infoBody').textContent = 'Body' + key;
+  var img = document.getElementById('infoImg');
+  img.src = `./assets/logos/${key.toLowerCase()}.jpg`;
+  img.style.width = '500px';
+  img.style.height = '200px';
+  document.getElementById('infoTitle').textContent = infoStands[key].title;
+  // document.getElementById('infoBody').textContent = 'Body' + key;
 }
 
 function fadeIn(el) {
@@ -45,6 +35,35 @@ function fadeOut(el) {
 
 window.changeAll = function () {
   allActive = !allActive;
+  if (allActive)
+    currentActives = [
+      'miraflores',
+      'santiago_de_surco',
+      'barranco',
+      'san_isidro',
+      'san_borja',
+      'jesus_maria',
+      'lince',
+      'magdalena',
+      'pueblo_libre',
+      'san_miguel',
+      'surquillo',
+      'smp',
+      'ate',
+      'la_victoria',
+      'brena',
+      'chorrillos',
+      'cercado_lima',
+      'independencia',
+      'carabayllo',
+      'chaclacayo',
+      'mala',
+      'pachacamac',
+      'san_luis',
+      'san_bartolo',
+      'bellavista',
+    ];
+  else currentActives = [];
   for (var [key, value] of Object.entries(distritos)) {
     var imgs = value.imgs;
     distritos[key].active = allActive;
@@ -61,23 +80,43 @@ window.changeAll = function () {
   }
 };
 
+function shouldStayActive(distrito, stand) {
+  for (var [key, value] of Object.entries(distritos)) {
+    if (key === distrito || !distritos[key].active) continue;
+
+    var imgs = value.imgs;
+    for (var imgId of imgs) {
+      if (imgId === stand) return true;
+    }
+  }
+  return false;
+}
+
 window.myFunction = function (distrito) {
   if (distrito === 'todos') {
     changeAll();
     return;
   }
   distritos[distrito].active = !distritos[distrito].active;
-  for (var [key, value] of Object.entries(distritos)) {
-    var imgs = value.imgs;
-    var active = value.active;
-    for (var imgId of imgs) {
-      var img = document.getElementById(imgId);
-      if (active) {
-        if (img.style.opacity != 1) fadeIn(img);
-        document.getElementById(key + '_check').textContent = '\u2705';
-      } else {
-        if (img.style.opacity != 0.2) fadeOut(img);
-        document.getElementById(key + '_check').textContent = '\u274C';
+  var active = distritos[distrito].active;
+  var imgs = distritos[distrito].imgs;
+
+  if (active) {
+    document.getElementById(distrito + '_check').textContent = '\u2705';
+    currentActives.push(distrito);
+  } else {
+    document.getElementById(distrito + '_check').textContent = '\u274C';
+    var filteredArray = currentActives.filter((e) => e !== distrito);
+    currentActives = filteredArray;
+  }
+
+  for (var imgId of imgs) {
+    var img = document.getElementById(imgId);
+    if (active) {
+      if (img.style.opacity != 1) fadeIn(img);
+    } else {
+      if (img.style.opacity != 0.2) {
+        if (!shouldStayActive(distrito, imgId)) fadeOut(img);
       }
     }
   }
